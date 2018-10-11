706fb22292bf792395b466d943f2289bff43b33b
anton <adeviatkin@intech-global.com>
2018-10-11 15:31:50 +0300

    Refactored class DishFileSystem

--- app/src/main/java/com/example/devyatkin/dishes/DishContentActivity.java
+++ app/src/main/java/com/example/devyatkin/dishes/DishContentActivity.java
@@ -2,10 +2,17 @@ package com.example.devyatkin.dishes;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -41,6 +48,5 @@ public class DishContentActivity extends AppCompatActivity {
             else
                 image.setImageResource(R.drawable.ic_noimage);
         }
-
     }
 }
